47d7aaa52bb364e4545c94f781d5cddca88ab553
dhuble1dd <palsanich99@gmail.com>
2022-12-21 14:32:29 +0300

    cringex2

--- ball.js
+++ ball.js
@@ -12,28 +12,161 @@ let gamma = 2;
 
 class Object {
     constructor(element){
-        this.element = element;       
-    }
-
-    showObject() {
+        this.element = element; 
         this.currentLeft = parseInt(window.getComputedStyle(this.element, Number).left);
         this.currentTop = parseInt(window.getComputedStyle(this.element, Number).top);
-        this.position = this.element.getBoundingClientRect();
-        
+        this.position = this.element.getBoundingClientRect();   
+        //this.top = this.position.y;
+        //this.bottom = this.top + this.position.height;
+        //this.left = this.position.x;
+        //this.right = this.left + this.position.width;  
+        //this.key = {
+        //    key1: 'KeyW',
+        //    key2: 'KeyS',
+        //    //key3: 'ArrowUp',
+        //    //key4: 'ArrowDown',
+        //} 
+        //return {position: this.position, 
+        //    currentTop: this.currentTop,
+        //    currentLeft: this.currentLeft,
+        //    top: this.top,
+        //    bottom: this.bottom,
+        //    left: this.left,
+        //    right:  this.right};
+    }
+
+    movePlate() {
+        document.addEventListener('keydown', (evt) => {
+            this.position = this.element.getBoundingClientRect();
+            this.currentTop = parseInt(window.getComputedStyle(this.element, Number).top); 
+            this.top = this.position.y;
+            this.bottom = this.top + this.position.height;
+            switch (this.element) {
+                case player1Platform:
+                    this.key1 = 'KeyW';
+                    this.key2 = 'KeyS';
+                    break;
+                case player2Platform:
+                    this.key1 = 'ArrowUp';
+                    this.key2 = 'ArrowDown';
+                    break;    
+            
+                default:
+                    break;
+            }
+            if (evt.code === this.key1) {
+                if(this.top <= 0)
+                    return;
+                this.element.style.top = this.currentTop - range + 'px';
+            }
+
+            if (evt.code === this.key2) {
+                if(this.bottom >= browseHeight)
+                    return;
+                this.element.style.top = this.currentTop + range + 'px';
+            } 
+        });   
+        return {x: this.position.x, y: this.position.y, w: this.position.width, h: this.position.height}; 
+    }
+
+    drawBall() {
+        this.currentLeft = parseInt(window.getComputedStyle(this.element, Number).left);
+        this.currentTop = parseInt(window.getComputedStyle(this.element, Number).top);
+        this.position = this.element.getBoundingClientRect();   
         this.top = this.position.y;
         this.bottom = this.top + this.position.height;
         this.left = this.position.x;
-        this.right = this.left + this.position.width;
-        return {position: this.position, 
-                currentTop: this.currentTop,
-                currentLeft: this.currentLeft,
-                top: this.top,
-                bottom: this.bottom,
-                left: this.left,
-                right:  this.right};
+        this.right = this.left + this.position.width; 
+        //const {x: left1, y: top1, w: width1, h: height1} = this.movePlate(player1Platform);
+        //const {x: left2, y: top2, w: width2, h: height2} = this.movePlate(player2Platform);
+
+        this.circle = {
+            x : this.left,
+            y : this.top,
+            w : this.position.width,
+            h : this.position.height,
+        }
+    
+    //    this.platform1 = {
+    //        x : left1,
+    //        y : top1,
+    //        w : width1,
+    //        h : height1,
+    //    }
+    
+    //    this.platform2 = {
+    //        x : left2,
+    //        y : top2,
+    //        w : width2,
+    //        h : height2,
+    //    }
+
+        return{
+            currentTop: this.currentTop,
+            currentLeft: this.currentLeft,
+            circle: this.circle,
+            //platform1: this.platform1,
+            //platform2: this.platform2,
+            ballTop:this.top, 
+            ballBottom: this.bottom, 
+            ballLeft: this.left, 
+            ballRight: this.right
+        }
     }
 
+    //moveBall(){
+    //    const {currentTop, currentLeft, circle, platform1, platform2, ballTop, ballBottom, ballLeft, ballRight} = this.drawBall();
+    //    if (crossObj(circle, platform1) || crossObj(circle, platform2)) 
+    //            delta = -delta;
+
+    //    if (ballTop < 0 || ballBottom >= browseHeight ) 
+    //            gamma = -gamma;
+
+    //        ball.style.left = currentLeft + delta + 'px';
+             
+    //        ball.style.top = currentTop + gamma + 'px';
+    
+    //    switch (random) {
+    //        case 1:
+    //            ball.style.left = currentLeft - delta + 'px';
+             
+    //            ball.style.top = currentTop - gamma + 'px';;
+    //            break;
+    //        case 2:
+    //            ball.style.left = currentLeft + delta + 'px';
+
+    //            ball.style.top = currentTop - gamma + 'px';
+    //            break;
+    //        case 3:
+    //            ball.style.left = currentLeft - delta + 'px';
+             
+    //            ball.style.top = currentTop + gamma + 'px';
+    //            break;
+    //        case 4:
+    //            ball.style.left = currentLeft + delta + 'px';
+             
+    //            ball.style.top = currentTop + gamma + 'px';
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    
+    //    if (ballLeft < 0 || ballRight > browseWidth ) {
+    //        backToWork();
+    //    }
+    //}
 };
+
+const plate1 = new Object(player1Platform);
+const plate2 = new Object(player2Platform);
+const ball0 = new Object(ball);
+
+const platform1 = plate1.movePlate();
+const platform2 = plate2.movePlate();
+console.log(platform1, platform2);
+//plate1.movePlate();
+//plate2.movePlate();
+
 //let ball0 = new Object(ball);
 //let plate1 = new Object(player1Platform);
 //let plate2 = new Object(player2Platform);
@@ -52,90 +185,88 @@ class Object {
 //    return objParam;
 //}
 
-function drawPlate(player) {
-    const plate = new Object(player);
-    let {currentTop,top, bottom} = plate.showObject();
+//function drawPlate(player) {
+//    const plate = new Object(player);
+//    let {currentTop,top, bottom} = plate.showObject();
 
-    return {currentTop,top, bottom};
-}
+//    return {currentTop,top, bottom};
+//}
 
-function movePlate(objectsToProcess) {
-    document.addEventListener('keydown', function(evt) {
-        if(objectsToProcess && Array.isArray(objectsToProcess))
-        {
-            const actualPlatform = 
-                objectsToProcess.find((e)=>e.key1 === evt.code || e.key2 === evt.code)
+//function movePlate(objectsToProcess) {
+//    document.addEventListener('keydown', function(evt) {
+//        if(objectsToProcess && Array.isArray(objectsToProcess))
+//        {
+//            const actualPlatform = 
+//                objectsToProcess.find((e)=>e.key1 === evt.code || e.key2 === evt.code)
         
-            if(actualPlatform){
-                const {platform, key1, key2} = actualPlatform;
-                const {currentTop, top, bottom} = drawPlate(platform);
+//            if(actualPlatform){
+//                const {platform, key1, key2} = actualPlatform;
+//                const {currentTop, top, bottom} = drawPlate(platform);
                 
-                if (evt.code === key1) {
-                    if(top <= 0)
-                        return;
-                    platform.style.top = currentTop - range + 'px';
-                }
+//                if (evt.code === key1) {
+//                    if(top <= 0)
+//                        return;
+//                    platform.style.top = currentTop - range + 'px';
+//                }
 
-                if (evt.code === key2) {
-                    if(bottom >= browseHeight)
-                        return;
-                    platform.style.top = currentTop + range + 'px';
-                }
-            }
+//                if (evt.code === key2) {
+//                    if(bottom >= browseHeight)
+//                        return;
+//                    platform.style.top = currentTop + range + 'px';
+//                }
+//            }
             
-        }
+//        }
         
-    });
-    return
-}
-const objectsToProcess = [];
-objectsToProcess.push({platform: player1Platform, key1: 'KeyW', key2: 'KeyS'})
-objectsToProcess.push({platform: player2Platform, key1: 'ArrowUp', key2: 'ArrowDown'})
+//    });
+//    return
+//}
+//const objectsToProcess = [];
+//objectsToProcess.push({platform: player1Platform, key1: 'KeyW', key2: 'KeyS'})
+//objectsToProcess.push({platform: player2Platform, key1: 'ArrowUp', key2: 'ArrowDown'})
 
-movePlate(objectsToProcess);
+//movePlate(objectsToProcess);
 
-function drawBall() {
-    const ball0 = new Object(ball);
-    const plate1 = new Object(player1Platform);
-    const plate2 = new Object(player2Platform);
+//function drawBall() {
+//    const ball0 = new Object(ball);
+//    const plate1 = new Object(player1Platform);
+//    const plate2 = new Object(player2Platform);
 
-    const {position: ballPosition,
-        currentTop, currentLeft,
-        top: ballTop,
-        bottom: ballBottom,
-        left: ballLeft,
-        right: ballRight} = ball0.showObject();
-    const {position: plate1Position, top: plate1Top, left: plate1Left} = plate1.showObject();
-    const {position: plate2Position, top: plate2Top, left: plate2Left} = plate2.showObject();
+//    const {position: ballPosition,
+//        currentTop, currentLeft,
+//        top: ballTop,
+//        bottom: ballBottom,
+//        left: ballLeft,
+//        right: ballRight} = ball0.showObject();
 
-    circle = {
-        x : ballLeft,
-        y : ballTop,
-        w : ballPosition.width,
-        h : ballPosition.height
-    }
+//    circle = {
+//        x : ballLeft,
+//        y : ballTop,
+//        w : ballPosition.width,
+//        h : ballPosition.height
+//    }
 
-    platform1 = {
-        x : plate1Left,
-        y : plate1Top,
-        w : plate1Position.width,
-        h : plate1Position.height
-    }
+//    platform1 = {
+//        x : plate1Left,
+//        y : plate1Top,
+//        w : plate1Position.width,
+//        h : plate1Position.height
+//    }
 
-    platform2 = {
-        x : plate2Left,
-        y : plate2Top,
-        w : plate2Position.width,
-        h : plate2Position.height
-    }
+//    platform2 = {
+//        x : plate2Left,
+//        y : plate2Top,
+//        w : plate2Position.width,
+//        h : plate2Position.height
+//    }
 
-    return{currentTop, currentLeft, circle, platform1, platform2, ballTop, ballBottom, ballLeft, ballRight}
-}
+//    return{currentTop, currentLeft, circle, platform1, platform2, ballTop, ballBottom, ballLeft, ballRight}
+//}
 
 function moveBall() {
     
-    const {currentTop, currentLeft, circle, platform1, platform2, ballTop, ballBottom, ballLeft, ballRight} = drawBall();
-
+    const {currentTop, currentLeft, circle, ballTop, ballBottom, ballLeft, ballRight} = ball0.drawBall();
+    
     if (crossObj(circle, platform1) || crossObj(circle, platform2)) 
                 delta = -delta;
 
@@ -197,4 +328,3 @@ function backToWork() {
     random = getRandom(1,4);
     alert('Продолжим?')
 }
-
--- ball2.js
+++ ball2.js
@@ -1,54 +1,216 @@
-// const button = document.querySelector('.button');
+//// const button = document.querySelector('.button');
 
-// const showMessage200 = props => alert(200);
+//// const showMessage200 = props => alert(200);
 
-// button.addEventListener('click', showMessage200);
+//// button.addEventListener('click', showMessage200);
 
-// class MovingObject{
-//     constructor(x, y, backgroundColor = 'red'){
-//         const square = document.createElement('div');
-//         square.style.position = 'absolute';
-//         square.style.width = '40px';
-//         square.style.height = '40px';
-//         square.style.backgroundColor = backgroundColor;
+//// class MovingObject{
+////     constructor(x, y, backgroundColor = 'red'){
+////         const square = document.createElement('div');
+////         square.style.position = 'absolute';
+////         square.style.width = '40px';
+////         square.style.height = '40px';
+////         square.style.backgroundColor = backgroundColor;
 
-//        this.x = x;
-//        this.y = y;
-//        this.element = square
-//     }
-//     showObject(){
-//         document.querySelector('body').appendChild(this.element);
-//         console.log(this.element.style.top)
-//         this.element.style.top = this.y + 'px';
-//         this.element.style.left = this.x + 'px';
-//     }
-// };
-
-
-// const hereMeOut = ()=>({
-//     a: 1,
-//     b: 'hello'
-// })
-
-// const {a:aa, b} = hereMeOut();
+////        this.x = x;
+////        this.y = y;
+////        this.element = square
+////     }
+////     showObject(){
+////         document.querySelector('body').appendChild(this.element);
+////         console.log(this.element.style.top)
+////         this.element.style.top = this.y + 'px';
+////         this.element.style.left = this.x + 'px';
+////     }
+//// };
 
 
-// const massive = [400, 500, 600, 700, 800];
-// const colors = ['blue', 'black', 'gray', 'green', 'gold']
+//// const hereMeOut = ()=>({
+////     a: 1,
+////     b: 'hello'
+//// })
 
-// massive.forEach((item, index) => {
-//     const movObj = new MovingObject(item, item, colors[index]);
-//     movObj.showObject();
-// })
+//// const {a:aa, b} = hereMeOut();
 
 
-const hello = document.querySelector('.helo')
+//// const massive = [400, 500, 600, 700, 800];
+//// const colors = ['blue', 'black', 'gray', 'green', 'gold']
 
-const bcr = hello.getBoundingClientRect();
+//// massive.forEach((item, index) => {
+////     const movObj = new MovingObject(item, item, colors[index]);
+////     movObj.showObject();
+//// })
 
-console.log('bcr is ', bcr)
 
-// this.top = this.position.y;
-// this.bottom = this.top + this.position.height;
-// this.left = this.position.x;
-// this.right = this.left + this.position.width;
+//const hello = document.querySelector('.helo')
+
+//const bcr = hello.getBoundingClientRect();
+
+//console.log('bcr is ', bcr)
+
+//// this.top = this.position.y;
+//// this.bottom = this.top + this.position.height;
+//// this.left = this.position.x;
+//// this.right = this.left + this.position.width;
+
+//let promise = new Promise((resolve, reject) => {
+//    setTimeout(() => resolve('done'), 1000);
+//    //setTimeout(() => reject(new Error('Whoooops!!!')), 1000);
+//});
+
+//promise.then(
+//    result => alert(result),
+//    error => alert(error)
+//)
+//promise.then(alert);
+//promise.then(null, alert)
+//promise.catch(alert);
+
+//new Promise((resolve, reject) => {
+//    //setTimeout(() => resolve('value'), 2000);
+//    throw new Error('error');
+//})
+
+//.finally(() => alert('Промис завершен'))
+////.then(alert);
+//.catch(alert)
+
+//function loadScript(src) {
+//    return new Promise((resolve, reject) => {
+//        let script = document.createElement('script');
+//        script.src = src;
+
+//        script.onload = () => resolve(script);
+//        script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
+
+//        document.head.append(script);
+//    })
+//}
+
+//let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+
+//promise.then(
+//    script => alert(`${script.src} загружен!`),
+//    error => alert(`Ошибка: ${error.message}`)
+//)
+
+//promise.then(script => alert('Еще один обработчик...'));
+
+//function delay(ms) {
+//    return new Promise(resolve => setTimeout(resolve, ms));
+//}
+
+//delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+//function go() {
+//    showCircle(300, 300, 100).then(div => {
+//      div.classList.add('message');
+//      div.append("Hello, world!");
+//    });
+//  }
+
+//  function showCircle(cx, cy, radius) {
+//    let div = document.createElement('div');
+//    div.style.width = 0;
+//    div.style.height = 0;
+//    div.style.left = cx + 'px';
+//    div.style.top = cy + 'px';
+//    div.className = 'circle';
+//    document.body.append(div);
+
+//    return new Promise(resolve => {
+//      setTimeout(() => {
+//        div.style.width = radius * 2 + 'px';
+//        div.style.height = radius * 2 + 'px';
+
+//        div.addEventListener('transitionend', function handler() {
+//          div.removeEventListener('transitionend', handler);
+//          resolve(div);
+//        });
+//      }, 0);
+//    })
+//  }
+
+//new Promise(function(resolve, reject) {
+
+//    setTimeout(() => resolve(1), 1000); 
+  
+//  }).then(function(result) { 
+  
+//    alert(result); 
+//    return result * 2;
+  
+//  }).then(function(result) { 
+  
+//    alert(result); 
+//    return result * 2;
+  
+//  }).then(function(result) {
+  
+//    alert(result); 
+//    return result * 2;
+  
+//  });
+
+//new Promise(function(resolve, reject) {
+
+//    setTimeout(() => resolve(1), 1000);
+  
+//  }).then(function(result) {
+  
+//    alert(result); 
+  
+//    return new Promise((resolve, reject) => { 
+//      setTimeout(() => resolve(result * 2), 1000);
+//    });
+  
+//  }).then(function(result) { 
+  
+//    alert(result); 
+  
+//    return new Promise((resolve, reject) => {
+//      setTimeout(() => resolve(result * 2), 1000);
+//    });
+  
+//  }).then(function(result) {
+  
+//    alert(result); 
+  
+//  });
+
+//Promise.any([
+//    new Promise((resolve, reject) => setTimeout(() => reject(new Error("Ошибка!")), 1000)),
+//    new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
+//    new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000))
+//  ]).then(alert); 
+
+//fetch('https://jsonplaceholder.typicode.com/todos/1')
+//  .then(response => response.json())
+//  .then(json => console.log(json))
+
+//axios.get("https://jsonplaceholder.typicode.com/todos/1")
+//  .then(response => console.log("response", response.data))
+
+//fetch("https://jsonplaceholder.typicode.com/posts", {
+//  method: "POST",
+//  body: JSON.stringify({
+//    title: "Title of post",
+//    body: "Post Body"
+//  })
+//})
+//  .then(response => {
+//    if (!response.ok) throw Error(response.statusText);
+//    return response.json();
+//  })
+//  .then(data => console.log(data))
+//  .catch(error => console.log(error));
+
+//axios
+//  .post("https://jsonplaceholder.typicode.com/posts", {
+//    title: "Title of post",
+//    body: "Body of post"
+//  })
+//  .then(response => console.log(response.data))
+//  .catch(error => console.log(error))
+
+
